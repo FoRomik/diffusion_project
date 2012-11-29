f476ff589e38bb7aea2e96d4606a98afe79b5546
Gustav Baardsen <gba@cma.uio.no>
2012-11-29 19:09:41 +0100

    The python code in part_g.py has been made a bit cleaner.

--- part_g.py
+++ part_g.py
@@ -3,11 +3,21 @@ The non-linear diffusion equation
 
 rho*du/dt = \nabla \dot (alpha(u)\nabla u) + f(x, t),
 
-with x \in Omega, t \in (0, T], is solved with only
-one Picard iteration at every time step.
+with x \in Omega, t \in (0, T], is solved with the
+Picard iteration method.
 
 The PDE is discretized in time using the Backward 
 Euler method. 
+
+Here we have chosen the function f(x, t) to satisfy
+the manufactured solution u(x, t) = x**2*(0.5 - x/3)*t,
+either for the case with only one Picard iteration
+(maxiter = 1) or with a full selfconsistent 
+calculation (typically maxiter = 10 is more than 
+enough).
+
+The code contains a nose test which checks that the
+convergence rate is 1.
 
 """
 
@@ -16,24 +26,29 @@ from dolfin import *
 from numpy import *
 from sys import *
 
+#     Ct:           Proportionality constant for dt
+#     Cx:           Proportionality constant for dx
+#     h_values:     List of convergence parameters h
+#     maxiter:      Maximum number of Picard iterations
+#     T:            Stopping time for simulation
 Ct = 0.1
 Cx = 0.01
-
-#h_values = [10.0, 5.0, 2.0, 1.0, 0.5, 0.01]#, 0.005, 0.001
 h_values = [0.1, 0.05, 0.02, 0.01, 0.005]
 errors = []
+max_iter = 30
+T = 1.0
 
 file_1 = open('x_u.dat','w')
 file_e = open('x_ue.dat','w')
 
 for h in h_values:
     
-    #     degree = degree of finite element approximation
-    #     n_elements = list of number of elements
-    #                  for each dimension
-    #     dim = physical space dimension
-    #     ref_domain = type of reference domain
-    #     V = test function space
+    #     degree:        Degree of finite element approximation
+    #     n_elements:    List of number of elements
+    #                    for each dimension
+    #     dim:           Physical space dimension
+    #     ref_domain:    Type of reference domain
+    #     V:             Test function space
     degree = 1
     Nx = int(round(1.0/sqrt(Cx*h)))
     n_elements = [Nx]
@@ -47,15 +62,14 @@ for h in h_values:
     #     formulation.     
     #
     #     Initial condition
-    #u0 = Expression('exp(-pi*pi*t)*cos(pi*x[0])', t=0.0)
     u0 = Expression('x[0]*x[0]*(0.5 - x[0]/3.0)*t', t=0.0)
+    #u0 = Expression('exp(-pi*pi*t)*cos(pi*x[0])', t=0.0)
     #u0 = Expression('t', t=0.0)
     u_1 = interpolate(u0, V)
     u_q1 = interpolate(u0, V)
     
-    #     dt = time step length
-    #     T = stopping time for simulation
-    T = 1.0
+    #     Nt:            Number of time steps
+    #     dt:            Time step length
     Nt = int(round(T/(Ct*h)))
     dt = T/Nt
     print 'Nx = ', Nx, ', dt = ', dt
@@ -71,38 +85,26 @@ for h in h_values:
     #     Source function determined by the manufactured
     #     solution u0 and the diffusion coefficient 
     #     alpha.
-    # f = Expression('-rho*pow(x[0], 3)/3.0 + rho*x[0]*x[0]/2.0 '\
-    #                    '+ 8.0*pow(t, 3)*pow(x[0], 7)/9.0 '\
-    #                    '- 28.0*pow(t, 3)*pow(x[0], 6)/9.0 '\
-    #                    '+ 7.0*pow(t, 3)*pow(x[0], 5)/2.0 '\
-    #                    '- 5.0*pow(t, 3)*pow(x[0], 4)/4.0 '\
-    #                    '+ 2*t*x[0] - t', t=0.0, rho=rho)
-
-    f = Expression('rho*x[0]*x[0]*(-2*x[0] + 3)/6 -'\
-                      '(-12*t*x[0] + 3*t*(-2*x[0] + 3))'\
-                      '*(pow(x[0], 4)*pow((-dt + t), 2)'\
-                      '*pow((-2*x[0] + 3), 2) + 36)/324'\
-                      '- (-6*t*x[0]*x[0] + 6*t*x[0]*(-2*x[0] + 3))'\
-                      '*(36*pow(x[0], 4)*pow((-dt + t), 2)*(2*x[0] - 3)'\
-                      '+ 36*pow(x[0], 3)*pow((-dt + t), 2)'\
-                      '*pow((-2*x[0] + 3), 2))'\
-                      '/5832', t=0.0, dt=dt, rho=rho)
-    
-    # f = Expression('8*dt*dt*t*pow(x[0], 7)/9 '\
-    #                    '- 28*dt*dt*t*pow(x[0], 6)/9 '\
-    #                    '+ 7*dt*dt*t*pow(x[0], 5)/2 '\
-    #                    '- 5*dt*dt*t*pow(x[0], 4)/4 '\
-    #                    '- 16*dt*t*t*pow(x[0], 7)/9 '\
-    #                    '+ 56*dt*t*t*pow(x[0], 6)/9 '\
-    #                    '- 7*dt*t*t*pow(x[0], 5) '\
-    #                    '+ 5*dt*t*t*pow(x[0], 4)/2 '\
-    #                    '- rho*pow(x[0], 3)/3 + rho*x[0]*x[0]/2 '\
-    #                    '+ 8*pow(t, 3)*pow(x[0], 7)/9 '\
-    #                    '- 28*pow(t, 3)*pow(x[0], 6)/9 '\
-    #                    '+ 7*pow(t, 3)*pow(x[0], 5)/2 '\
-    #                    '- 5*pow(t, 3)*pow(x[0], 4)/4'\
-    #                    '+ 2*t*x[0] - t', t=0.0, dt=dt, rho=rho)
-    
+    if max_iter == 1:
+        #     Case with one Picard iteration
+        f = Expression('rho*x[0]*x[0]*(-2*x[0] + 3)/6 -'\
+                           '(-12*t*x[0] + 3*t*(-2*x[0] + 3))'\
+                           '*(pow(x[0], 4)*pow((-dt + t), 2)'\
+                           '*pow((-2*x[0] + 3), 2) + 36)/324'\
+                           '- (-6*t*x[0]*x[0] + 6*t*x[0]*(-2*x[0] + 3))'\
+                           '*(36*pow(x[0], 4)*pow((-dt + t), 2)*(2*x[0] - 3)'\
+                           '+ 36*pow(x[0], 3)*pow((-dt + t), 2)'\
+                           '*pow((-2*x[0] + 3), 2))'\
+                           '/5832', t=0.0, dt=dt, rho=rho)
+    else:
+        #     Case with self-consistent Picard solution
+        f = Expression('-rho*pow(x[0], 3)/3.0 + rho*x[0]*x[0]/2.0 '\
+                           '+ 8.0*pow(t, 3)*pow(x[0], 7)/9.0 '\
+                           '- 28.0*pow(t, 3)*pow(x[0], 6)/9.0 '\
+                           '+ 7.0*pow(t, 3)*pow(x[0], 5)/2.0 '\
+                           '- 5.0*pow(t, 3)*pow(x[0], 4)/4.0 '\
+                           '+ 2*t*x[0] - t', t=0.0, rho=rho)
+            
     #     Diffusion coefficient
     def alpha(u):
         return 1.0 + u**2
@@ -118,7 +120,6 @@ for h in h_values:
     u = Function(V)
     t = dt
     tol = 1E-10
-    max_iter = 1
     #     Loop over time
     while t <= T:
         f.t = t
@@ -129,49 +130,47 @@ for h in h_values:
         
         error = 1.0
         iteration = 0
-        #     Picard iteration to solve the nonlinear
+        #     Picard iterations to solve the nonlinear
         #     equation.
         while error > tol and iteration < max_iter:
             iteration += 1 
+            
             #     Assemble the matrix and vector
             A = assemble(a)
             b = assemble(L)
-            #     Solve the linear system of equations.
+            #     Solve the linear system of equations
             solve(A, u.vector(), b)
             
             #     Calculate the relative difference
-            #     of u vector 2-norms.
+            #     of u vector 2-norms
             norm_diff = linalg.norm(u.vector().array() \
                                         - u_q1.vector().array())
             norm_u = max(linalg.norm(u_q1.vector().array()), \
                              0.00000001)
             error = norm_diff/norm_u
-            #print 'rel_diff=',error
+            
             #     Update u_q1
             u_q1.assign(u)
         
-        #print 'iter = ', iteration
-        #print 'rel error = ', error
-        
         #     Test the converged result against the 
-        #     exact manufactured solution
+        #     exact manufactured solution u0
         u0.t = t
         u_e = interpolate(u0, V)
         e = u_e.vector().array() - u.vector().array()
         E = sqrt(sum(e**2)/u.vector().array().size)
         
-        #print 'error = ', E
         if t+dt > T: 
             errors.append(E)
-            #print 't+dt=',t+dt,',T=',T
-    
-            uv = u.vector().array()
-            x = zeros(len(uv))
-            for i in range(len(uv)): 
-                file_1.write(str(mesh.coordinates()[i][0])
-                             +' '+str(u.vector().array()[i])+'\n')
-                file_e.write(str(mesh.coordinates()[i][0])
-                             +' '+str(u_e.vector().array()[i])+'\n')
+            
+            #     Write numerical and exact solutions
+            #     to file
+            # uv = u.vector().array()
+            # x = zeros(len(uv))
+            # for i in range(len(uv)): 
+            #     file_1.write(str(mesh.coordinates()[i][0])
+            #                  +' '+str(u.vector().array()[i])+'\n')
+            #     file_e.write(str(mesh.coordinates()[i][0])
+            #                  +' '+str(u_e.vector().array()[i])+'\n')
             
         t += dt
         u_1.assign(u)
@@ -190,9 +189,9 @@ for i in range(1, nh):
 
 #     Check that convergence rate is 1 with a nose
 #     test
-#diff = abs(r[nh-2]-1.0)
-#print 'diff = ', diff
-#nt.assert_almost_equal(diff, 0, delta=1E-2)
+diff = abs(r[nh-2]-1.0)
+print 'diff = ', diff
+nt.assert_almost_equal(diff, 0, delta=1E-1)
 
 file_1.close()
 file_e.close()
